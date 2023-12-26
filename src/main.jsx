2fc82fc0ff1be5241894ba65b2f66ce4396bccc3
Sandeep Sharma <sandeepsharma2183@gmail.com>
2023-12-26 08:23:59 +0530

    up arrow button added

--- src/main.jsx
+++ src/main.jsx
@@ -2,17 +2,14 @@ import React, { Suspense } from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
-import { CartProvider } from './context/cart.jsx'
 import { Provider } from 'react-redux'
 import store from './store.jsx'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <CartProvider>
       <Provider store={store}>
         <App />
       </Provider>
-    </CartProvider>
   </React.StrictMode>,
 )
 
