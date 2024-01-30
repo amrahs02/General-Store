c4044c6183f6213c929fb96dab563476dee7e523
Sandeep Kumar Sharma <65103353+amrahs02@users.noreply.github.com>
2024-01-30 23:54:26 +0530

    Revert "borders fixed"

--- src/components/Navigation/BottomNav.jsx
+++ src/components/Navigation/BottomNav.jsx
@@ -14,63 +14,52 @@ export default function LabelBottomNavigation() {
   };
 
   return (
-    <div className='sm:!hidden !bg-slate-800 flex items-center justify-evenly rounded-t-xl w-full fixed bottom-0' value={value} onChange={handleChange}>
-      <Link to='/' className='relative  text-gray-300'>
+    <BottomNavigation className='sm:!hidden !bg-slate-600 pt-6 rounded-t-xl w-full fixed bottom-0' showLabels value={value} onChange={handleChange}>
+      <Link to='/' className='relative bottom-10 text-gray-300'>
         <BottomNavigationAction
           label="Home"
           icon={
-            <Link to="/" className=' text-gray-300 rounded-full '>
-              <div className='bg-blue-500 rounded-full px-4 py-1' >
-
-                <Home />
-              </div>
+            <Link to="/" className=' text-gray-300 hover:bg-gray-800 active:bg-slate-800 px-5 my-1 rounded-full '>
+              <Home />
               <h5>Home</h5>
             </Link>}
         />
       </Link>
-      <Link to='/sudhadairy' className='relative  text-gray-300'>
+      <Link to='/sudhadairy' className='relative bottom-10 text-gray-300'>
         <BottomNavigationAction
           label="Sudha"
           icon={
-            <Link to="/sudhadairy" className='text-gray-300    rounded-full '>
-              <div className='bg-blue-500 rounded-full px-4 py-1' >
-
-                <Store />
-              </div>
+            <Link to="/sudhadairy" className='text-gray-300 hover:bg-gray-800 active:bg-slate-800 px-5 my-1   rounded-full '>
+              <Store />
               <h5>Sudha</h5>
 
             </Link>}
         />
       </Link>
-      <Link to='/xerox' className='relative   text-gray-300'>
+      <Link to='/xerox' className='relative bottom-10  text-gray-300'>
         <BottomNavigationAction
           label="Print"
           icon={
-            <Link to="/xerox" className='text-gray-300   rounded-full'>
-              <div className='bg-blue-500 rounded-full px-4 py-1' >
-
-                <Print />
-              </div>
+            <Link to="/xerox" className='text-gray-300  hover:bg-gray-800 active:bg-slate-800 px-5 my-1 rounded-full'>
+              <Print />
               <h5>Print</h5>
 
             </Link>}
         />
       </Link>
-      <Link to='/colddrinks' className='relative  text-gray-300'>
+      <Link to='/colddrinks' className='relative bottom-10  text-gray-300'>
         <BottomNavigationAction
           label="Drinks"
           icon={
-            <Link to="/colddrinks" className='text-gray-300   rounded-full '>
-              <div className='bg-blue-500 rounded-full px-4 py-1' >
-
-                <LocalDrink />
-              </div>
+            <Link to="/colddrinks" className='text-gray-300  hover:bg-gray-800 active:bg-slate-800 px-5 my-1 rounded-full '>
+              <LocalDrink
+              />
               <h5>Drink</h5>
 
             </Link>}
         />
       </Link>
 
-    </div>
+    </BottomNavigation>
   );
 }
